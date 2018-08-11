e1f1f58954411f04e245eaa695c9db18784c981a
gentom <gento.m.developer@gmail.com>
2018-08-11 23:40:43 +0900

    update template

--- framework/element.js
+++ framework/element.js
@@ -1,6 +1,20 @@
+/*
 const div = (strings, ...args) => console.log(strings, args);
 
-const firstName = "Marvin";
-const lastName = "Frachet";
+const firstName = "Gento";
+const lastName = "Morikawa";
 
 div`Hello ${firstName} ${lastName} !`;
+*/
+
+const div = (strings, ...args) =>
+  strings.reduce(
+    (acc, currentString, index) => acc + currentString + (args[index] || ""),
+    ""
+  );
+
+const firstName = "Gento";
+const lastName = "Morikawa";
+
+const template = div`Hello ${firstName} ${lastName} !`;
+console.log(template); // It prints `Hello Marvin Frachet !`
