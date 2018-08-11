01e42c246d013ed6bb4a4686633cd3b57608a568
gentom <gento.m.developer@gmail.com>
2018-08-11 23:42:51 +0900

    Abstracting for others HTML elements

--- framework/element.js
+++ framework/element.js
@@ -7,6 +7,7 @@ const lastName = "Morikawa";
 div`Hello ${firstName} ${lastName} !`;
 */
 
+/*
 const div = (strings, ...args) =>
   strings.reduce(
     (acc, currentString, index) => acc + currentString + (args[index] || ""),
@@ -18,3 +19,22 @@ const lastName = "Morikawa";
 
 const template = div`Hello ${firstName} ${lastName} !`;
 console.log(template); // It prints `Hello Marvin Frachet !`
+*/
+
+const createElement = tagName => (strings, ...args) => ({
+    type: tagName, // this will be useful for the next chapter
+    template: strings.reduce(
+      (acc, currentString, index) => acc + currentString + (args[index] || ""),
+      ""
+    )
+  });
+  
+  const div = createElement("div");
+  const p = createElement("p");
+  
+  const firstName = "Gento";
+  const lastName = "Morikawa";
+  
+  //const template = div`Hello ${firstName} ${lastName} !`;
+  const { template } = p`Hello ${firstName} ${lastName} !`;
+  console.log(template);
